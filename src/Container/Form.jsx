d791208f70ca0dee7dc14547ab101dde6dda998b
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-02-15 00:13:25 +0100

    updating

--- src/Container/Form.jsx
+++ src/Container/Form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import React from "react";
+import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import BeatLoader from "react-spinners/BeatLoader"
@@ -13,7 +14,6 @@ const Form = () => {
         fullName: '',
         email: ''
     })
-
 
     const receiveUserEmail = (event) => {
         setUserData((prevData) => {
@@ -31,16 +31,32 @@ const Form = () => {
         if (userData.email.length <= 0) {
             setCheckBlankField(true)
         } else {
-            toast("wow")
+            let config = {
+                method : "POST",
+                url: import.meta.env.VITE_ACCESS_URL,
+                header : {
+                    "Content-Type" : "application/json"
+                },
+                data : userData
+            }
+            axios(config).then(response =>{
+                console.log(response.data)
+                toast("wow")
             console.log(userData.email)
             setValue(false)
+            }).catch(err =>{
+                console.log(err)
+                setValue(true)
+            })
+            
         }
 
     }
 
     return (
         <div className="w-4/5 m-auto md:w-1/2 md:mt-20">
-            <form className=" mt-8 flex md:mt-10">
+            <form className=" mt-8 flex  md:mt-10">
+                <input type="name" name="Full-name" id="full-name" className="bg-gray-300 h-16 w-2/3 p-4" placeholder="ENTER YOUR Full Name" onChange={receiveUserEmail} />
                 <input type="email" name="Email" id="Email" className="bg-gray-300 h-16 w-2/3 p-4" placeholder="ENTER YOUR EMAIL" onChange={receiveUserEmail} />
                 {checkBlankField && userData.email.length == 0 ?
                 <label className="text-red-600 font-Montserrat">Name cant be blank</label>: " "
