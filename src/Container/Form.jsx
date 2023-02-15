9fae1b9cde18debc687e00fea87b7caea4b9b91c
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-02-15 22:57:53 +0100

    edited

--- src/Container/Form.jsx
+++ src/Container/Form.jsx
@@ -63,21 +63,22 @@ const Form = () => {
             //     })
             // }
             let config = {
-                method: "POST",
+                method : "POST",
                 url: 'https://waitlist.cyclic.app/api',
-                header: {
-                    "Content-Type": "application/json"
+                header : {
+                    "Content-Type" : "application/json"
                 },
-                data: userData
+                data : userData
             }
-            axios(config).then(response => {
+            axios(config).then(response =>{
                 toast(response.data.fullName + ' ' + "thank you for joining our waitlist")
                 setValue(true)
+                console.log(response)
                 setUserData({
                     fullName: '',
                     email: ''
                 })
-            }).catch(err => {
+            }).catch(err =>{
                 toast('REGISTRATION FAILED')
                 console.log(err)
                 setValue(true)
@@ -98,20 +99,20 @@ const Form = () => {
                     <label className="text-red-600 font-Montserrat">Name cant be blank</label> : " "
                 }
                 <input type="name" name="Full-name" id="full-name" className="bg-gray-300 h-16 w-full p-4 m-auto mb-5" placeholder="ENTER YOUR FULL NAME" onChange={receiveUserFullname} value={userData.fullName || ''} />
-
-                {checkBlankField && userData.email.length == 0 ?
+                
+                 {checkBlankField && userData.email.length == 0 ?
                     <label className="text-red-600 font-Montserrat">Email cant be blank</label> : " "
                 }
-                <div className="flex flex-row">
-                    <input type="email" name="Email" id="Email" className="bg-gray-300 h-16 w-2/3 p-4 m-auto mb-5" placeholder="ENTER YOUR EMAIL" onChange={receiveUserEmail} value={userData.email || ''} />
-
-                    {initialValue ?
-                        <input type="submit" value="Notify Me" className="bg-Balablue-blue  w-1/3 h-16   text-white font-Gothic font-normal text-xs sm:text-xl " placeholder="ENTER YOUR EMAIL" onClick={receiveUserInput} /> :
-                        <div className="bg-Balablue-blue w-1/3 h-16 flex justify-center align-middle pt-4 ">
-                            <BeatLoader />
-                        </div>
-                    }
-                </div>
+               <div className="flex flex-row">
+               <input type="email" name="Email" id="Email" className="bg-gray-300 h-16 w-2/3 p-4 m-auto mb-5" placeholder="ENTER YOUR EMAIL" onChange={receiveUserEmail} value={userData.email || ''} />
+               
+                {initialValue ?
+                    <input type="submit" value="Notify Me" className="bg-Balablue-blue  w-1/3 h-16   text-white font-Gothic font-normal text-xs sm:text-xl " placeholder="ENTER YOUR EMAIL" onClick={receiveUserInput} /> :
+                    <div className="bg-Balablue-blue w-1/3 h-16 flex justify-center align-middle pt-4 ">
+                        <BeatLoader />
+                    </div>
+                }
+               </div>
             </form>
             <ToastContainer />
             <h3 className="text-center mt-6 font-light text-Balablue-blue md:mt-3">Notify Me When the App is lauched</h3>
